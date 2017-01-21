303edb7105124b0df41c6c74ed5b98516f4262b4
Cong Chen <congchen@mxic.com.cn>
2017-01-21 21:40:59 +0800

    Add delays between mysql container startup and app container startup, as mysql needs time to setup configs and dbs first time.

--- clean.sh
+++ clean.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Cleaning website codes, mysql dbs and httpd logs..."
 rm -rf webroot/qiqisdress.com
-rm -rf mysql/data/*
-rm -rf apache-php/logs/*
+rm -rf mysql/data
+rm -rf apache-php/logs
 echo "Done!"
